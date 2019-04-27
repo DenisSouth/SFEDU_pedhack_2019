f81b09e6c34aadae66e5b745c1ece09ca141ee5d
DenisSouth <31843379+DenisSouth@users.noreply.github.com>
2019-04-27 10:50:28 +0300

    Update tapermonkey.js

--- tapermonkey.js
+++ tapermonkey.js
@@ -1,6 +1,20 @@
 // в учительском интерфейсе приложение подсвечивает и подписывает вид воспрития у каждого ребенка
 
-var colors = {
+
+// ==UserScript==
+// @name         education yandex
+// @namespace    https://education.yandex.ru
+// @version      0.1
+// @description  try to take over the world!
+// @author       You
+// @match        https://education.yandex.ru/
+// @match   *//education.yandex.ru/*
+// @include	*://*.education.yandex.ru/*
+// @include	*://education.yandex.ru/*
+// ==/UserScript==
+
+setTimeout(function() {
+    var colors = {
     vis: "#e19bef",
     aud: "#95c8ed",
     kin: "#6cff59"
@@ -13,10 +27,11 @@ var lables = {
 }
 
 var data = '{"вася" : "kin","петя" : "vis","коля" : "vis","сашa" : "kin","ира" : "aud","гена" : "aud"}'
-json = JSON.parse(data);
-keys = Object.keys(json)
+var json = JSON.parse(data);
+var keys = Object.keys(json)
 var elems = document.getElementsByClassName("course-journal__table-body")[0];
 var names = elems.getElementsByClassName("course-journal__table-cell_student");
+var ind;
 for (ind in names) {
     if (typeof(names[ind].innerText) != "undefined") {
         console.log(names[ind].innerText.length);
@@ -27,8 +42,13 @@ for (ind in names) {
             if (names[ind].innerText.toLowerCase().includes(key) == true) {
                 names[ind].style.backgroundColor = colors[val];
 				names[ind].style.backgroundColor = colors[val];
-				names[ind].innerText = names[ind].innerText  + ' ' + lables[val];
+				names[ind].innerText = names[ind].innerText + ' ' + lables[val];
             }
         }
     }
 }
+}, 1000);
+
+
+
+
